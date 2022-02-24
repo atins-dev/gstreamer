88cc0a6800b2200c245a7ca9c828cb844505e930
powerory <powerory@atins.io>
2022-02-24 12:25:51 +0900

    rtspconnection, rtspdefs: Fix to support qop=auth
    *qop=auth-int is not supported yet.

--- subprojects/gst-plugins-base/gst-libs/gst/rtsp/gstrtspdefs.c
+++ subprojects/gst-plugins-base/gst-libs/gst/rtsp/gstrtspdefs.c
@@ -639,7 +639,7 @@ auth_digest_compute_response (const gchar * algorithm, const gchar * method,
   g_checksum_update (context, (const guchar *) realm, strlen (realm));
   g_checksum_update (context, (const guchar *) ":", 1);
   g_checksum_update (context, (const guchar *) password, strlen (password));
-  digest_string = g_strdup (g_checksum_get_string (context));
+  digest_string = g_checksum_get_string (context);
   g_assert (strlen (digest_string) == hash_len);
   memcpy (hex_a1, digest_string, hash_len);
 
@@ -699,7 +699,8 @@ done:
 gchar *
 gst_rtsp_generate_digest_auth_response (const gchar * algorithm,
     const gchar * method, const gchar * realm, const gchar * username,
-    const gchar * password, const gchar * uri, const gchar * nonce)
+    const gchar * password, const gchar * uri, const gchar * nonce,
+    const gchar * qop, const gchar * cnonce, const gchar * nc)
 {
   gchar *ret = NULL;
   g_return_val_if_fail (method != NULL, NULL);
@@ -710,9 +711,7 @@ gst_rtsp_generate_digest_auth_response (const gchar * algorithm,
   g_return_val_if_fail (nonce != NULL, NULL);
 
   ret = auth_digest_compute_response (algorithm, method,
-      realm, username, password, uri, nonce,
-      /* FIXME: send real values */
-      NULL, NULL, NULL);
+      realm, username, password, uri, nonce, qop, cnonce, nc);
 
   return ret;
 }
