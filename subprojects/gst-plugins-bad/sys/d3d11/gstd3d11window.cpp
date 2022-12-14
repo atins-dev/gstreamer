065fd7a7a153579ceb71463bd36394e9839e6d98
Seungha Yang <seungha@centricular.com>
2022-12-15 01:15:10 +0900

    d3d11videosink: Fix deadlock when parent window is busy
    Deadlock sequence:
    * From a streaming thread, d3d11videosink sends synchronous message
      to the parent window, so that internal (child) window can be
      constructed on the parent window's thread
    * App thread (parent window thread) is waiting for pipeline's
      state change (to GST_STATE_NULL) but streaming thread is
      blocked and waiting for app thread
    
    To avoid the deadlock, GstD3D11WindowWin32 should send message
    to the parent window asynchronously.

--- subprojects/gst-plugins-bad/sys/d3d11/gstd3d11window.cpp
+++ subprojects/gst-plugins-bad/sys/d3d11/gstd3d11window.cpp
@@ -109,7 +109,7 @@ static GstFlowReturn gst_d3d111_window_present (GstD3D11Window * self,
     ID3D11RenderTargetView * rtv);
 static void gst_d3d11_window_on_resize_default (GstD3D11Window * window,
     guint width, guint height);
-static gboolean gst_d3d11_window_prepare_default (GstD3D11Window * window,
+static GstFlowReturn gst_d3d11_window_prepare_default (GstD3D11Window * window,
     guint display_width, guint display_height, GstCaps * caps,
     gboolean * video_processor_available, DXGI_FORMAT display_format, GError ** error);
 
@@ -430,14 +430,14 @@ typedef struct
   gboolean supported;
 } GstD3D11WindowDisplayFormat;
 
-gboolean
+GstFlowReturn
 gst_d3d11_window_prepare (GstD3D11Window * window, guint display_width,
     guint display_height, GstCaps * caps, gboolean * video_processor_available,
     DXGI_FORMAT display_format, GError ** error)
 {
   GstD3D11WindowClass *klass;
 
-  g_return_val_if_fail (GST_IS_D3D11_WINDOW (window), FALSE);
+  g_return_val_if_fail (GST_IS_D3D11_WINDOW (window), GST_FLOW_ERROR);
 
   klass = GST_D3D11_WINDOW_GET_CLASS (window);
   g_assert (klass->prepare != NULL);
@@ -449,7 +449,7 @@ gst_d3d11_window_prepare (GstD3D11Window * window, guint display_width,
       video_processor_available, display_format, error);
 }
 
-static gboolean
+static GstFlowReturn
 gst_d3d11_window_prepare_default (GstD3D11Window * window, guint display_width,
     guint display_height, GstCaps * caps, gboolean * video_processor_available,
     DXGI_FORMAT display_format, GError ** error)
@@ -509,7 +509,7 @@ gst_d3d11_window_prepare_default (GstD3D11Window * window, guint display_width,
     GST_ERROR_OBJECT (window, "Cannot determine render format");
     g_set_error (error, GST_RESOURCE_ERROR, GST_RESOURCE_ERROR_FAILED,
         "Cannot determine render format");
-    return FALSE;
+    return GST_FLOW_ERROR;
   }
 
   if (display_format != DXGI_FORMAT_UNKNOWN) {
@@ -528,7 +528,7 @@ gst_d3d11_window_prepare_default (GstD3D11Window * window, guint display_width,
       g_set_error (error, GST_RESOURCE_ERROR, GST_RESOURCE_ERROR_FAILED,
           "Cannot determine render format");
 
-      return FALSE;
+      return GST_FLOW_ERROR;
     }
   } else {
     for (guint i = 0; i < GST_VIDEO_INFO_N_COMPONENTS (&window->info); i++) {
@@ -798,12 +798,12 @@ gst_d3d11_window_prepare_default (GstD3D11Window * window, guint display_width,
 
   GST_DEBUG_OBJECT (window, "New swap chain 0x%p created", window->swap_chain);
 
-  return TRUE;
+  return GST_FLOW_OK;
 
 error:
   gst_d3d11_device_unlock (window->device);
 
-  return FALSE;
+  return GST_FLOW_ERROR;
 }
 
 void
