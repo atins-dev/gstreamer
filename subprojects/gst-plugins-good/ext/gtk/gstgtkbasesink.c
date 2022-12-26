atins-dev/gstreamer

commit 941c0e81ddf5c0ad6a9fac59b1d6bf7723025434

gtksink: Return reference to GtkWidget in the acquire function
This should ensure thread safety.

Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/1088>

subprojects/gst-plugins-good/ext/gtk/gstgtkbasesink.c
@@ -178,7 +178,7 @@ static GtkGstBaseWidget *
 gst_gtk_base_sink_get_widget (GstGtkBaseSink * gtk_sink)
 {
   if (gtk_sink->widget != NULL)
-    return gtk_sink->widget;
+    return g_object_ref (gtk_sink->widget);
 
   /* Ensure GTK is initialized, this has no side effect if it was already
    * initialized. Also, we do that lazily, so the application can be first */
@@ -211,7 +211,7 @@ gst_gtk_base_sink_get_widget (GstGtkBaseSink * gtk_sink)
   gtk_gst_base_widget_set_element (GTK_GST_BASE_WIDGET (gtk_sink->widget),
       GST_ELEMENT (gtk_sink));
 
-  return gtk_sink->widget;
+  return g_object_ref (gtk_sink->widget);
 }
 
 GtkWidget *
@@ -221,7 +221,7 @@ gst_gtk_base_sink_acquire_widget (GstGtkBaseSink * gtk_sink)
 
   GST_OBJECT_LOCK (gtk_sink);
   if (gtk_sink->widget != NULL)
-    widget = gtk_sink->widget;
+    widget = g_object_ref (gtk_sink->widget);
   GST_OBJECT_UNLOCK (gtk_sink);
 
   if (!widget)
@@ -241,7 +241,7 @@ gst_gtk_base_sink_get_property (GObject * object, guint prop_id,
   switch (prop_id) {
     case PROP_WIDGET:
     {
-      g_value_set_object (value, gst_gtk_base_sink_acquire_widget (gtk_sink));
+      g_value_take_object (value, gst_gtk_base_sink_acquire_widget (gtk_sink));
       break;
     }
     case PROP_FORCE_ASPECT_RATIO:

subprojects/gst-plugins-good/ext/gtk/gstgtkglsink.c
@@ -136,11 +136,14 @@ gst_gtk_gl_sink_set_property (GObject * object, guint prop_id,
 {
   switch (prop_id) {
     case PROP_ROTATE_METHOD:
-      gtk_gst_gl_widget_set_rotate_method (GTK_GST_GL_WIDGET
-          (gst_gtk_base_sink_acquire_widget (GST_GTK_BASE_SINK (object))),
+    {
+      GtkWidget *widget =
+          gst_gtk_base_sink_acquire_widget (GST_GTK_BASE_SINK (object));
+      gtk_gst_gl_widget_set_rotate_method (GTK_GST_GL_WIDGET (widget),
           g_value_get_enum (value), FALSE);
+      g_object_unref (widget);
       break;
-
+    }
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
       break;
@@ -154,11 +157,16 @@ gst_gtk_gl_sink_get_property (GObject * object, guint prop_id,
 {
   switch (prop_id) {
     case PROP_ROTATE_METHOD:
-      g_value_set_enum (value,
-          gtk_gst_gl_widget_get_rotate_method (GTK_GST_GL_WIDGET
-              (gst_gtk_base_sink_acquire_widget (GST_GTK_BASE_SINK (object)))));
-      break;
+    {
+      GtkWidget *widget =
+          gst_gtk_base_sink_acquire_widget (GST_GTK_BASE_SINK (object));
 
+      g_value_set_enum (value,
+          gtk_gst_gl_widget_get_rotate_method (GTK_GST_GL_WIDGET (widget)));
+      g_object_unref (widget);
+
+      break;
+    }
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
       break;
@@ -454,6 +462,8 @@ gst_gtk_gl_sink_event (GstBaseSink * sink, GstEvent * event)
             (gst_gtk_base_sink_acquire_widget (GST_GTK_BASE_SINK (sink)));
 
         gtk_gst_gl_widget_set_rotate_method (widget, orientation, TRUE);
+
+        g_object_unref (widget);
       }
       break;
     default:

commit 30dfcbb7239b003ff591f00e160a128aebdc2c91

gtkbasesink: Fix widget leak
gst_gtk_base_sink_get_widget() will increase refcount and it should
be released after use

Fixing regression introduced by the commit
941c0e81ddf5c0ad6a9fac59b1d6bf7723025434

subprojects/gst-plugins-good/ext/gtk/gstgtkbasesink.c
@@ -230,7 +230,8 @@ gst_gtk_base_sink_get_widget (GstGtkBaseSink * gtk_sink)
 
   /* Take the floating ref, other wise the destruction of the container will
    * make this widget disappear possibly before we are done. */
-  gst_object_ref_sink (gtk_sink->widget);
+  g_object_ref_sink (gtk_sink->widget);
+
   gtk_sink->widget_destroy_id = g_signal_connect (gtk_sink->widget, "destroy",
       G_CALLBACK (widget_destroy_cb), gtk_sink);
 
@@ -339,6 +340,7 @@ gst_gtk_base_sink_navigation_send_event (GstNavigation * navigation,
     gtk_gst_base_widget_display_size_to_stream_size (widget,
         x, y, &stream_x, &stream_y);
     gst_navigation_event_set_coordinates (event, stream_x, stream_y);
+    g_object_unref (widget);
   }
 
   pad = gst_pad_get_peer (GST_VIDEO_SINK_PAD (sink));
@@ -370,8 +372,10 @@ gst_gtk_base_sink_start_on_main (GstBaseSink * bsink)
   GstGtkBaseSink *gst_sink = GST_GTK_BASE_SINK (bsink);
   GstGtkBaseSinkClass *klass = GST_GTK_BASE_SINK_GET_CLASS (bsink);
   GtkWidget *toplevel;
+  GtkGstBaseWidget *widget;
 
-  if (gst_gtk_base_sink_get_widget (gst_sink) == NULL) {
+  widget = gst_gtk_base_sink_get_widget (gst_sink);
+  if (!widget) {
     GST_ERROR_OBJECT (bsink, "Could not ensure GTK initialization.");
     return FALSE;
   }
@@ -392,6 +396,8 @@ gst_gtk_base_sink_start_on_main (GstBaseSink * bsink)
     gst_sink->window_destroy_id = g_signal_connect (gst_sink->window, "destroy",
         G_CALLBACK (window_destroy_cb), gst_sink);
   }
+
+  g_object_unref (widget);
 
   return TRUE;
 }